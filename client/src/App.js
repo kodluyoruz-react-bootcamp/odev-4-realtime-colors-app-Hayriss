c241b9c0646a79866c0a9236ca3e0864addc56a8
Hayriss <bhyariye93@gmail.com>
2021-01-22 17:07:05 +0300

    create client

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import { initSocket,disconnectSocket, emitColor, subscribeToColor, subscribeInitialColor } from "./socketService";
+
+function App() {
+  const [color, setColor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(color);
+    emitColor(color);  //color backende iletildi
+  };
+
+  useEffect(() => {
+    initSocket();
+
+    subscribeInitialColor((data) => {
+      setColor(data);
+    });
+
+
+    subscribeToColor((color) => {
+      setColor(color);
+    });
+
+
+    return () => disconnectSocket();
+  }, [setColor])
+
+
+  return <div style={{ backgroundColor: `${color}` }} className="App">
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={(e) => setColor(e.target.value)}
+        type="color"
+        value={color}
+        name="color"
+      />
+      <br />
+      <button >Color</button>
+      <p>Your hex color code is : {color}</p>
+    </form>
+  </div>
+}
+
+export default App;
---
+++ client/src/socketService.js
@@ -0,0 +1,38 @@
+import { io } from 'socket.io-client';
+
+let socket;
+
+export const initSocket = () => {
+    socket = io('http://localhost:3000', {
+        transports: ['websocket']
+    })
+    console.log("connecting...")
+    socket.on("connect", () => console.log("connected"));
+}
+
+export const disconnectSocket = () => {
+	console.log("disconnecting...");
+	if (socket) socket.disconnect();
+};
+
+export const emitColor = (color) => {
+    if (socket) socket.emit("new-color", color);    
+};
+
+export const subscribeToColor = (cb) => {
+    if(!socket) return true;
+
+    socket.on('receive-color',(color) => {
+        console.log("color received:",color);
+        cb(color)
+    })
+}
+
+export const subscribeInitialColor = (cb) => {
+    if (!socket) return true;
+  
+    socket.on("color-received", (data) => {
+      console.log("color received from other client", data);
+      cb(data);
+    });
+  };
